dbe14c7b1eb26b1690a1b9161c1d00a09e6c2389
Evgeniy <zigzagmed@yandex.ru>
2023-01-23 22:43:43 +0300

    ДЗ к семинару №3

---
+++ task19/Program.cs
@@ -0,0 +1,67 @@
+﻿// ДЗ. Напишите программу, которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом.
+
+// Вариант 1 (через string)
+
+Console.WriteLine("Вариант 1. Введите пятизначное число: ");
+string num = Convert.ToString(Console.ReadLine());
+int len = num.ToString().Length;
+
+if (len == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine(num + " - палиндром");
+    }
+    else
+    {
+        Console.WriteLine(num + " - не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+
+
+// Вариант 2 (через int)
+
+Console.WriteLine("Вариант 2. Введите пятизначное число: ");
+int five = Convert.ToInt32(Console.ReadLine());
+while (five < 10000 || five > 99999) //Проверяем правильность введения числа
+    {
+        Console.WriteLine("Вы ввели неверное число. Введите пятизначное число: ");
+        five = Convert.ToInt32(Console.ReadLine());
+    }
+if ((five / 10000) % 10 != five % 10 || (five / 1000) % 10 != (five / 10) % 10)
+    {
+        Console.WriteLine(five + " - не является палиндромом");
+    }
+    else if ((five / 1000) % 10 != (five / 10) % 10)
+    {
+        Console.WriteLine(five + " - не является палиндромом");
+    }
+else 
+    {
+        Console.WriteLine(five + " - палиндром");
+    }
+
+
+// 3 Вариант с циклом for внутри метода, для числа любой длины
+
+string palindrom (int N) // Создаем метод
+{
+    int len = N.ToString().Length;
+    string newN = Convert.ToString(N);
+    for (int i = 0; i < len/2; i++) // Цикл для проверки зеркальных значений
+        {
+            if (newN[i] != newN[len - 1 - i]) return " - не палиндром";
+        }
+    return " - палиндром";
+}
+
+Console.WriteLine("Вариант 3. Введите число для проверки на палиндром: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write(N + palindrom(N));
+
+// Вариант 3 мне больше нравится
---
+++ task21/Program.cs
@@ -0,0 +1,42 @@
+﻿// ДЗ. 21. Напишите программу, которая принимает на вход координаты
+//  координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B(2,1,-7) -> 15,84  A (7,-5,0); B(1,-1,9) -> 11,53
+
+
+// Вариант 1
+double distance3D () // Метод, который выдает результат
+{
+string [,] array = {{"x1", "y1", "z1"}, {"x2", "y2", "z2"}};
+int [,] coordin = new int [2, 3];
+for (int i = 0; i < 2; i++)
+    for (int j = 0; j < 3; j++)
+    {
+        Console.WriteLine("Введите " + array[i, j]);
+        coordin [i, j] = Convert.ToInt32(Console.ReadLine()); // Запоминаем координаты
+    }
+double result = Math.Sqrt(Math.Pow((coordin [0, 0]-coordin [1, 0]), 2) + Math.Pow((coordin [0, 1]-coordin [1, 1]), 2) + Math.Pow((coordin [0, 2]-coordin [1, 2]), 2));
+return result;
+}
+
+double result = distance3D();
+Console.WriteLine("Расстояние между точками = " + result);
+
+
+
+// Вариант 2
+
+Console.Write("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result2 = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2) + Math.Pow((z1-z2), 2));
+Console.WriteLine("Расстояние между точками = " + result2);
